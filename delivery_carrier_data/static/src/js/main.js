0c76b3e98b010ab46721fdd5e2fbcb5c12d3dbd0
Haojun Zou <apollo_zhj@msn.com>
2017-05-19 11:10:23 +0200

    delivery_carrier_data: bugfix

--- delivery_carrier_data/static/src/js/main.js
+++ delivery_carrier_data/static/src/js/main.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
 
     // When choosing an delivery carrier, update the quotation and the acquirers
     var $carrier = $("#delivery_carrier");
-    $carrier.find("*[name='carrier_data']").change(function (ev) {
+    $carrier.find("[name='carrier_data']").change(function (ev) {
         var carrier_data = $(ev.currentTarget).val();
         var carrier_id = $(ev.currentTarget).closest('label').find("input[name='delivery_type']").val();
-        openerp.jsonRpc("/shop/delivery/data_input", 'call', {
+        openerp.jsonRpc("/shop/delivery/carrier_data", 'call', {
             'carrier_id': carrier_id,
             'carrier_data': carrier_data,
         })
