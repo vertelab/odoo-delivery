491a7d7d92dea4a6d8e429462864fa9a9f38fffc
Haojun Zou <apollo_zhj@msn.com>
2017-11-03 14:37:38 +0100

    website_sale_delivery_json: new module

---
+++ website_sale_delivery_json/controllers.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2017- Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import openerp
+from openerp import http
+from openerp.http import request
+from openerp.addons.website_sale_delivery.controllers.main import website_sale
+import logging
+_logger = logging.getLogger(__name__)
+
+class website_sale(website_sale):
+
+    @http.route(['/shop/payment/delivery_update'], type='json', auth='public', website=True)
+    def delivery_update(self, carrier_id, **kw):
+        order = request.website.sale_get_order()
+        if order and carrier_id:
+            order.carrier_id = int(carrier_id)
+            order.delivery_set()
+        return {'amount_total': self.formatted_price(order.amount_total), 'amount_tax': self.formatted_price(order.amount_tax), 'amount_delivery': self.formatted_price(order.amount_delivery)}
+
+    def formatted_price(self, price):
+        lang = request.env['res.lang'].search([('code', '=', request.env.user.lang)])
+        if len(lang) > 0:
+            price = '%.2f' %price
+            return price.replace('.', lang.decimal_point)
+        return price
---
+++ website_sale_delivery_json/static/src/js/website_sale_delivery.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    // When choosing an delivery carrier, update the quotation and the acquirers
+    var $carrier = $("#delivery_carrier");
+    $carrier.find("input[name='delivery_type']").click(function (ev) {
+        var carrier_id = $(ev.currentTarget).val();
+        openerp.jsonRpc("/shop/payment/delivery_update", "call", {
+            'carrier_id': carrier_id,
+        }).done(function(data){
+            $("#order_total").find("span.oe_currency_value").replaceWith('<span class="oe_currency_value">' + data['amount_total'] + '</span>');
+            $("#order_total_taxes").find("span.oe_currency_value").replaceWith('<span class="oe_currency_value">' + data['amount_tax'] + '</span>');
+            $("#order_delivery").find("span.oe_currency_value").replaceWith('<span class="oe_currency_value">' + data['amount_delivery'] + '</span>');
+        });
+    });
+
+});
