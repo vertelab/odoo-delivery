bb2dc3d8c9740cc187a1556f7c1536422a371077
Robin Chatfield <robin.chatfield@vertel.se>
2019-12-16 14:20:41 +0100

    delivery_pricelist: Fixed endless loop in name_get/get_grid.

--- delivery_pricelist/delivery.py
+++ delivery_pricelist/delivery.py
@@ -20,6 +20,7 @@
 ##############################################################################
 
 import openerp.exceptions
+from openerp.osv import osv
 from openerp import models, fields, api, _
 from openerp import http
 from openerp.http import request
@@ -28,27 +29,45 @@ import openerp.addons.website_sale.controllers.main
 import logging
 _logger = logging.getLogger(__name__)
 
-
-
-class delivery_carrier(models.Model):
-    _inherit = "delivery.carrier"
-    @api.multi
-    def grid_get(self,contact_id):
-        contact = self.env['res.partner'].browse(contact_id)
-        for carrier in self:
-            res = super(delivery_carrier,carrier).grid_get(contact_id)
-            if res:
-                for grid in carrier.grids_id:
-                    get_id = lambda x: x.id
-                    pricelist_ids = map(get_id, grid.pricelist_ids)
-                    if pricelist_ids and not contact.property_product_pricelist.id in pricelist_ids:
-                        continue
-                    _logger.warn('%s %s' % (grid,pricelist_ids))
-                    return grid.id
+class delivery_carrier(osv.osv):
+    _inherit = 'delivery.carrier'
+    
+    def name_get(self, cr, uid, ids, context=None):
+        # name_get goes into an endless loop when we use contact.property_product_pricelist.id
+        # in grid_get (name_get uses get_price, which uses grid_get, and
+        # then its back to name_get). Limiting to one extra pass through here.
+        if context and context.get('delivery_carrier_name_get_done'):
+            result = []
+            for record in self.read(cr, uid, ids, ['id', 'name'], context=context):
+                result.append((record['id'], record['name']))
+            return result
+        context = dict(context or {})
+        context['delivery_carrier_name_get_done'] = True
+        return super(delivery_carrier, self).name_get(cr, uid, ids, context=context)
+    
+    def grid_get(self, cr, uid, ids, contact_id, context=None):
+        contact = self.pool.get('res.partner').browse(cr, uid, contact_id, context=context)
+        for carrier in self.browse(cr, uid, ids, context=context):
+            for grid in carrier.grids_id:
+                get_id = lambda x: x.id
+                country_ids = map(get_id, grid.country_ids)
+                state_ids = map(get_id, grid.state_ids)
+                pricelist_ids = map(get_id, grid.pricelist_ids)
+                if country_ids and not contact.country_id.id in country_ids:
+                    continue
+                if state_ids and not contact.state_id.id in state_ids:
+                    continue
+                if grid.zip_from and (contact.zip or '')< grid.zip_from:
+                    continue
+                if grid.zip_to and (contact.zip or '')> grid.zip_to:
+                    continue
+                if grid.pricelist_ids and contact.property_product_pricelist.id in pricelist_ids:
+                    continue
+                return grid.id
         return False
 
 class delivery_grid(models.Model):
-    _inherit = "delivery.grid"
+    _inherit = 'delivery.grid'
 
     pricelist_ids = fields.Many2many(comodel_name="product.pricelist",string="Pricelists",help="Delivery options for customers that have these pricelists.")
 
