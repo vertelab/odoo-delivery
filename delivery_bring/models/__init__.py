d6ba937111db962397d260ba810b566fa18e4e53
iammiracle <miracleaayodele@gmail.com>
2024-02-29 10:49:57 +0100

    remove bring modules

--- delivery_bring/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import delivery_bring
-from . import product_packaging
-from . import inherited_stock_picking
-from . import inherited_sale_order
-# from . import bring_request
--- delivery_bring_fixed_price/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import inherited_delivery_bring
