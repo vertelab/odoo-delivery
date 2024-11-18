11a8ca167ab1a297d309a4dc84a6f15da89ab07c
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2024-11-18 12:21:38 +0000

    delivery_carrier_data: ported. The javascript does not work yet

---
+++ delivery_carrier_data/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Odoo, Open Source Management Solution, third party addon
+# Copyright (C) 2021- Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Delivery Carrier Data',
+    'version': '1.0',
+    'summary': 'Add extra input field to delivery method',
+    'category': 'stock',
+    'description': """
+Show input field in check out form in website_sale
+================================================== 
+Financed by Cavarosa LTD
+Is meant to be inherited and other modules will fill the input field.
+""",
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': [
+        'delivery', 'website', 'sale', 'website_sale', 
+    ],
+    'data': [
+        'views/delivery_view.xml'
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'delivery_carrier_data/static/src/js/main.js',
+        ],
+    },
+    'installable': True,
+}
