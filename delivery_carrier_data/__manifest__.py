71d7213872ea62a88602730f80d5c2b3394c9d73
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-12-23 11:52:07 +0100

    Update __manifest__.py

--- delivery_carrier_data/__manifest__.py
+++ delivery_carrier_data/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 # Odoo, Open Source Management Solution, third party addon
-# Copyright (C) 2021- Vertel AB (<http://vertel.se>).
+# Copyright (C) 2024- Vertel AB (<http://vertel.se>).
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -31,7 +31,8 @@ Is meant to be inherited and other modules will fill the input field.
 """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
-    'website': 'http://www.vertel.se',
+    'website': 'https://vertel.se/apps/odoo-delivery/delivery_carrier_data',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'depends': [
         'delivery', 'website', 'sale', 'website_sale', 
     ],
