ef7b0c45473989085dfa0914d0ca58a849981141
iammiracle <miracleaayodele@gmail.com>
2025-10-21 13:41:32 +0200

    [T/6295]delivery_fraktjakt_barcode - Fraktjakt query after confriming in barcode interface

---
+++ delivery_fraktjakt_barcode/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Delivery Fraktjakt Barcodes",
+    "summary": "Set Fraktjakt Shipment Details via Barcode UI",
+    "version": "16.0.1.1.0",
+    "author": "Vertel AB",
+    "website": "https://vertel.se",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": ["stock_barcodes", "delivery_fraktjakt"],
+    "data": [
+        # "wizard/stock_barcodes_read_picking_views.xml",
+    ],
+    "installable": True,
+}
---
+++ delivery_fraktjakt_barcode/wizard/stock_barcodes_read_picking.py
@@ -0,0 +1,40 @@
+import logging
+
+from odoo import _, api, fields, models
+from odoo.tools.safe_eval import safe_eval
+
+_logger = logging.getLogger(__name__)
+
+
+class WizStockBarcodesReadPicking(models.TransientModel):
+    _inherit = "wiz.stock.barcodes.read.picking"
+
+    def action_validate_picking(self):
+        # for candidate_picking in self.candidate_picking_ids:
+        valid, result = self.candidate_picking_ids.with_context(
+            wiz_barcode_id=self.id,
+            picking_id=self.picking_id.id,
+            skip_sms=True,
+            skip_immediate=True,
+        ).action_validate_picking()
+        if not valid:
+            return result
+
+        # fraktjakt shipment query
+        return self.picking_id.fraktjakt_query()
+
+        # action = self.env["ir.actions.actions"]._for_xml_id(
+        #     "stock_barcodes.stock_barcodes_action_picking_tree_ready"
+        # )
+        #
+        # if self.picking_id and self.picking_id.picking_type_id:
+        #     context = self.env.context.copy()
+        #     context.update(safe_eval(action["context"]))
+        #     context.update(
+        #         {"search_default_picking_type_id": self.picking_id.picking_type_id.id}
+        #     )
+        #     action["context"] = context
+        #
+        # return action
+
+
